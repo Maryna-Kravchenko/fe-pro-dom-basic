61f7177868020a867aded9d7f969c8354011f8dc
Maryna Kravchenko <6302421@gmail.com>
2022-07-21 23:34:57 -0600

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  const arr = [...document.getElementsByClassName('item')];
+  arr.map((value, index) => {
+    if (index % 2 === 0) return (value.style.backgroundColor = 'red');
+  });
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+  const arr = document.getElementsByClassName('item');
+  let elem = document.querySelector('li');
+
+  for (let i = 0; i < arr.length; i++) {
+    if (elem.classList.contains('likedItem')) {
+      elem.style.backgroundColor = 'blue';
+    } else {
+      elem = elem.nextElementSibling;
+    }
+  }
+};
